5d7141a6610c62020cad815c8cf5a46f579e9e15
Leo Gaskin <leo.gaskin@le0.gs>
2023-08-30 15:34:53 +0200

    Commit improved window-switching bindings

--- extension.js
+++ extension.js
@@ -30,19 +30,52 @@
 const Meta = imports.gi.Meta;
 const Main = imports.ui.main;
 const Shell = imports.gi.Shell;
+const Clutter = imports.gi.Clutter;
 const ExtensionUtils = imports.misc.extensionUtils;
 
 const Me = ExtensionUtils.getCurrentExtension();
 
 function get_focus_window() {
-    let workspace = (global.screen || global.workspace_manager).get_active_workspace();
-    let windows = workspace.list_windows();
-    for ( let i = 0; i < windows.length; ++i ) {
-        if (windows[i].has_focus()) {
-            return windows[i];
-        }
+    return global.display.focus_window;
+}
+
+function absolute_to_relative(x, y, rect) {
+    let rel_x = (x - rect.x) / rect.width;
+    let rel_y = (y - rect.y) / rect.height;
+
+    return [rel_x, rel_y];
+}
+
+function relative_to_absolute(x, y, rect) {
+    let abs_x = (x * rect.width) + rect.x;
+    let abs_y = (y * rect.height) + rect.y;
+
+    return [abs_x, abs_y];
+}
+
+function remember_pointer_for_window(window) {
+    let rect = window.get_buffer_rect();
+    let [mouse_x, mouse_y, _] = global.get_pointer();
+    let [rel_x, rel_y] = absolute_to_relative(mouse_x, mouse_y, rect);
+
+    window.__zen_window_coords = [rel_x, rel_y];
+}
+
+function restore_pointer_for_window(window) {
+    let rect = window.get_buffer_rect();
+    let seat = Clutter.get_default_backend().get_default_seat();
+
+    if (seat === null) {
+        return;
     }
-    return null;
+
+    if (window.__zen_window_coords) {
+        let [rel_x, rel_y] = window.__zen_window_coords;
+        let [abs_x, abs_y] = relative_to_absolute(rel_x, rel_y, rect);
+        seat.warp_pointer(abs_x, abs_y);
+    } else {
+        seat.warp_pointer(rect.x + rect.width / 2, rect.y + rect.height / 2);
+    }
 }
 
 function switchWindow(next) {
@@ -99,39 +132,45 @@ function switchWindow(next) {
                  windows[target_idx].get_window_type() == Meta.WindowType.MODAL_DIALOG);
     }
 
-    Main.activateWindow(windows[target_idx]);
+    let window = windows[target_idx];
+
+    remember_pointer_for_window(current_window);
+    Main.activateWindow(window);
+    restore_pointer_for_window(window);
 }
 
 class Extension {
-  constructor() {
-    this.settings = ExtensionUtils.getSettings();
-  }
+    constructor() {
+        this.settings = ExtensionUtils.getSettings();
+    }
 
-  enable() {
-    // Switch to the next/previous window within this workspace. Window
-    // ordering is fixed (not based on most recent use like the alt-tab
-    // switcher) so it can match a window list shown by e.g. the
-    // Frippery Bottom Panel.
-    Main.wm.addKeybinding("switch-window-next-workspace",
-        this.settings,
-        Meta.KeyBindingFlags.NONE,
-        Shell.ActionMode ? Shell.ActionMode.NORMAL : Shell.KeyBindingMode.NORMAL,
-        function(display, screen, window, binding) {
-            switchWindow(true);
-        }
-    );
-    Main.wm.addKeybinding("switch-window-prev-workspace",
-        this.settings,
-        Meta.KeyBindingFlags.NONE,
-        Shell.ActionMode ? Shell.ActionMode.NORMAL : Shell.KeyBindingMode.NORMAL,
-        function(display, screen, window, binding) {
-            switchWindow(false);
-        }
-    );
-  }
+    enable() {
+        // Switch to the next/previous window within this workspace. Window
+        // ordering is fixed (not based on most recent use like the alt-tab
+        // switcher) so it can match a window list shown by e.g. the
+        // Frippery Bottom Panel.
+        Main.wm.addKeybinding("switch-window-next-workspace",
+            this.settings,
+            Meta.KeyBindingFlags.NONE,
+            Shell.ActionMode ? Shell.ActionMode.NORMAL : Shell.KeyBindingMode.NORMAL,
+            function(display, screen, window, binding) {
+                switchWindow(true);
+            }
+        );
+        Main.wm.addKeybinding("switch-window-prev-workspace",
+            this.settings,
+            Meta.KeyBindingFlags.NONE,
+            Shell.ActionMode ? Shell.ActionMode.NORMAL : Shell.KeyBindingMode.NORMAL,
+            function(display, screen, window, binding) {
+                switchWindow(false);
+            }
+        );
+    }
 
-  disable() {
-  }
+    disable() {
+        Main.wm.removeKeybinding("switch-window-next-workspace");
+        Main.wm.removeKeybinding("switch-window-prev-workspace");
+    }
 }
 
 function init() {
