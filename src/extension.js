2dc55cb4c46e929cc9f38661e46beb7461d3d7fd
Leo Gaskin <leo.gaskin@le0.gs>
2023-09-01 01:26:16 +0200

    Commit settings menu to disable different features

--- src/extension.js
+++ src/extension.js
@@ -4,15 +4,32 @@ const Me = ExtensionUtils.getCurrentExtension();
 const DirectWindowSwitch = Me.imports.direct_window_switch;
 const FocusNewWindow = Me.imports.focus_new_window;
 
+function conditionallyEnable(settings, key, extension) {
+    settings.get_boolean(key) ? extension.enable() : null;
+
+    settings.connect(`changed::${key}`, (settings, key) => {
+        settings.get_boolean(key) ? extension.enable() : extension.disable();
+    });
+}
+
 class Extension {
     constructor() {
-        this.direct_window_switch = new DirectWindowSwitch.Extension()
-        this.focus_new_window = new FocusNewWindow.Extension()
+        this.settings = ExtensionUtils.getSettings();
+        this.direct_window_switch = new DirectWindowSwitch.Extension();
+        this.focus_new_window = new FocusNewWindow.Extension();
     }
 
     enable() {
-        this.direct_window_switch.enable();
-        this.focus_new_window.enable();
+        conditionallyEnable(
+            this.settings,
+            "enable-direct-window-switch",
+            this.direct_window_switch,
+        );
+        conditionallyEnable(
+            this.settings,
+            "enable-focus-new-window",
+            this.focus_new_window,
+        );
     }
 
     disable() {
---
+++ src/prefs.js
@@ -0,0 +1,102 @@
+const { Adw, Gtk, Gio } = imports.gi;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+
+const { gettext: _ } = ExtensionUtils;
+
+/**
+ * Like `extension.js` this is used for any one-time setup like translations.
+ *
+ * @param {object} metadata - The metadata.json file, parsed as JSON
+ */
+function init(metadata) {
+  console.debug(`initializing ${metadata.name} Preferences`);
+
+  ExtensionUtils.initTranslations();
+}
+
+/**
+ * This function is called when the preferences window is first created to build
+ * and return a GTK4 widget.
+ *
+ * The preferences window will be a `Adw.PreferencesWindow`, and the widget
+ * returned by this function will be added to an `Adw.PreferencesPage` or
+ * `Adw.PreferencesGroup` if necessary.
+ *
+ * @returns {Gtk.Widget} the preferences widget
+ */
+function buildPrefsWidget() {
+  return new Gtk.Label({
+    label: Me.metadata.name,
+  });
+}
+
+/**
+ * This function is called when the preferences window is first created to fill
+ * the `Adw.PreferencesWindow`.
+ *
+ * If this function is defined, `buildPrefsWidget()` will NOT be called.
+ *
+ * @param {Adw.PreferencesWindow} window - The preferences window
+ */
+function fillPreferencesWindow(window) {
+  const settings = ExtensionUtils.getSettings();
+
+  const prefsPage = new Adw.PreferencesPage({
+    name: "general",
+    title: _("General"),
+    icon_name: "dialog-information-symbolic",
+  });
+  window.add(prefsPage);
+
+  const group = new Adw.PreferencesGroup({
+    title: _("Features"),
+    description: _("Select any features you want to have enabled"),
+  });
+  prefsPage.add(group);
+
+  {
+    const row = new Adw.ActionRow({
+      title: _("Direct window switch"),
+      subtitle: _(
+        "Enables Super-TAB and Super-Shift-TAB bindings for quick window switching",
+      ),
+    });
+    group.add(row);
+
+    const toggle = new Gtk.Switch({
+      valign: Gtk.Align.CENTER,
+    });
+    row.add_suffix(toggle);
+    row.set_activatable_widget(toggle);
+
+    settings.bind(
+      "enable-direct-window-switch",
+      toggle,
+      "active",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
+  }
+
+  {
+    const row = new Adw.ActionRow({
+      title: _("Focus new window"),
+      subtitle: _("Enables automatic focusing of newly created windows"),
+    });
+    group.add(row);
+
+    const toggle = new Gtk.Switch({
+      valign: Gtk.Align.CENTER,
+    });
+    row.add_suffix(toggle);
+    row.set_activatable_widget(toggle);
+
+    settings.bind(
+      "enable-focus-new-window",
+      toggle,
+      "active",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
+  }
+}
