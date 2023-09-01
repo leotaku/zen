de660186fd6694655c22b44afe034155b4e0e087
Leo Gaskin <leo.gaskin@le0.gs>
2023-09-01 08:41:09 +0200

    Commit .dir-locals.el for indentation settings

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((js-indent-level . 4)))
+ (nxml-mode . ((nxml-attribute-indent . 4)
+               (nxml-child-indent . 4))))
